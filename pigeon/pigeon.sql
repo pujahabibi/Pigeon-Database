8ec5803bf4d45062283b3b6168c3142592db3cb4
Puja Ahmad Habibi <pujahabibi07@gmail.com>
2017-07-10 19:56:12 +0700

    Add files via upload

---
+++ pigeon/pigeon.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.11
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 10, 2017 at 07:54 AM
+-- Server version: 5.6.21
+-- PHP Version: 5.6.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `pigeon`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_pigeon`
+--
+
+CREATE TABLE IF NOT EXISTS `data_pigeon` (
+  `nomor_ring` varchar(200) NOT NULL,
+  `tanggal_lahir` varchar(200) NOT NULL,
+  `jenis_kelamin` varchar(200) NOT NULL,
+  `riwayat` varchar(200) NOT NULL,
+  `prestasi` text NOT NULL,
+  `gambar` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
